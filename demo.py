a62a54efa04a232b9f21de24b71e798dbf2ed303
Tejasv <devtejasv1996@gmail.com>
2017-07-12 20:21:24 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,16 @@
+import sys
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
+
+
+clf = clf.fit(X, Y)
+a = map(int, raw_input().strip().split(' '))
+prediction = clf.predict(a)
+
+print(prediction)
